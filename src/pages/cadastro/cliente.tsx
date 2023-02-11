3922668809bfbe55ad6b2f6d6626577f4f7c130d
Dianine <dianine23@gmail.com>
2023-02-10 22:19:40 -0300

    Continuacao montagem da tela de cadstro de cliente

--- src/pages/cadastro/cliente.tsx
+++ src/pages/cadastro/cliente.tsx
@@ -30,17 +30,13 @@ function createData(
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Talisson Félix', 159, 6.0, 24, 4.0),
+  createData('Marcelo Vieira', 237, 9.0, 37, 4.3),
+  createData('Rai de Macedo', 262, 16.0, 24, 6.0),
 ];
 
 function Cliente() {
-
   const [open, setOpen] = useState(true);
-
   const handleClick = () => {
     setOpen(!open);
   };
@@ -56,90 +52,100 @@ function Cliente() {
         <Image src="/Logo.png" width={95} height={95} alt="Logo AcesseNet" />
         <h1 className="text-stone-50 font-bold text-3xl"> AcesseNet</h1>
         <h3 className="text-stone-500 font-semibold text-lg">Cadastro de cliente</h3>
-        <div className="w-full px-4 py-3">
-
-          <List
-            className=''
-            sx={{ width: '100%', bgcolor: 'background.paper' }}
-            component="nav"
-            aria-labelledby="nested-list-subheader"
-          >
-            <ListItemButton onClick={handleClick}>
-              <ListItemIcon>
-              </ListItemIcon>
-              <ListItemText primary="Cadastro de cliente" />
-              {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <List component="div" disablePadding>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <form className="flex flex-col justify-items-center">
-                    <div>
-                      <p className="text-stone-100 font-semibold text-base pt-8">Nome do cliente</p>
-                      <span className="flex row items-center pl-1.5">
-                        <Image src="/icons/User.svg" className="mr-[-32px] z-10" width={24} height={24} alt="Logo AcesseNet" />
-                        <input
-                          className="rounded p-2 pl-10 items-center my-2 w-80 text-stone-100 bg-stone-900"
-                          type="text"
-                          placeholder="Maria Cardoso Santos"
-                        />
-                      </span>
-                    </div>
-                    <div className="flex row justify-center">
-                      <button className="p-2 mt-8 rounded w-80 font-semibold bg-blue-500 hover:bg-blue-300" >Cadastrar</button>
-                    </div>
-                  </form>
+        <div className="w-full px-4 py-3 justify-center">
+          <div className='flex justify-center gap-10'>
+            {/* <div>
+              <List
+                className='bg-black  text-stone-100 w-[500px] justify-center'
+              >
+                <ListItemButton onClick={handleClick} className='bg-stone-900 hover:bg-slate-600'>
+                  <ListItemText className='text-stone-100 font-semibold text-base'>
+                    Cadastro de Cliente
+                  </ListItemText>
+                  {open ? <ExpandLess /> : <ExpandMore />}
                 </ListItemButton>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    <ListItemButton sx={{ pl: 4 }}>
+                      <form className="flex flex-col justify-items-center">
+                        <div>
+                          <p className="text-stone-100 font-semibold text-base">Nome do cliente</p>
+                          <span className="flex row items-center pl-1.5">
+                            <Image src="/icons/User.svg" className="mr-[-32px] z-10" width={24} height={24} alt="icone usuário" />
+                            <input
+                              className="rounded p-2 pl-10 items-center my-2 w-80 text-stone-100 bg-stone-900"
+                              type="text"
+                              placeholder="Maria Cardoso Santos"
+                            />
+                          </span>
+                        </div>
+                        <div className="flex row justify-center">
+                          <button className="p-2 mt-4 rounded w-80 font-semibold bg-blue-500 hover:bg-blue-300" >Cadastrar</button>
+                        </div>
+                      </form>
+                    </ListItemButton>
+                  </List>
+                </Collapse>
               </List>
-            </Collapse>
-          </List>
-          <br />
-          <div>
-            <span className="flex row items-center pl-1.5">
-              <Image src="/icons/Search.svg" className="mr-[-32px] z-10" width={24} height={24} alt="Logo AcesseNet" />
-              <input
-                className="rounded p-2 pl-10 items-center my-2 w-80 text-stone-100 bg-stone-900"
-                type="text"
-                placeholder="Pesquisa"
-              />
-            </span>
+            </div> */}
+            <div className='flex gap-10 items-center justify-between'>
+              <form className="flex justify-items-center gap-10">
+                <div>
+                  <p className="text-stone-100 font-semibold text-base">Nome do cliente</p>
+                  <span className="flex row items-center pl-1.5">
+                    <Image src="/icons/User.svg" className="mr-[-32px] z-10" width={24} height={24} alt="icone usuário" />
+                    <input
+                      className="rounded p-2 pl-10 items-center my-2 w-80 text-stone-100 bg-stone-900"
+                      type="text"
+                      placeholder="Maria Cardoso Santos"
+                    />
+                  </span>
+                </div>
+                <div className="flex row justify-center">
+                  <button className="p-2 mt-8 rounded w-40 h-10 font-semibold bg-blue-500 hover:bg-blue-300" >Cadastrar</button>
+                </div>
+              </form>
+              <div>
+                <p className="text-stone-100 font-semibold text-base">Pesquisa</p>
+                <span className="flex row items-center pl-1.5">
+                  <Image src="/icons/Search.svg" className="mr-[-32px] z-10" width={24} height={24} alt="icone usuário" />
+                  <input
+                    className="rounded p-2 pl-10 items-center my-2 w-80 text-stone-100 bg-stone-900"
+                    type="text"
+                    placeholder="Pesquisa"
+                  />
+                </span>
+              </div>
+            </div>
           </div>
-
-
-
-
-
-
-
-
-
-
-
-
-          <TableContainer component={Paper}>
+          <TableContainer component={Paper} className='mt-4 text-stone-100 bg-stone-900 border-r-8 border-l-8'>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Dessert (100g serving)</TableCell>
-                  <TableCell align="right">Calories</TableCell>
-                  <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                  <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                  <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableHead >
+                <TableRow >
+                  <TableCell className='text-stone-100'>Nome do cliente</TableCell>
+                  <TableCell className='text-stone-100' align="right"></TableCell>
+                  <TableCell className='text-stone-100' align="right"></TableCell>
                 </TableRow>
               </TableHead>
-              <TableBody>
+              <TableBody >
                 {rows.map((row) => (
                   <TableRow
                     key={row.name}
                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                   >
-                    <TableCell component="th" scope="row">
+                    <TableCell className='text-stone-100' component="th" scope="row">
                       {row.name}
                     </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                    <TableCell className='text-stone-100' align="right">
+                      <button className="p-2 mt-4 rounded w-20 font-semibold bg-blue-500 hover:bg-blue-300" >
+                        Editar
+                      </button>
+                    </TableCell>
+                    <TableCell className='text-stone-100' align="right">
+                      <button className="p-2 mt-4 rounded w-20 font-semibold bg-blue-500 hover:bg-blue-300" >
+                        Editar
+                      </button>
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
