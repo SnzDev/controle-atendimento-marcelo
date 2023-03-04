8c28866841b52bff2942825f4e886c2f04ba2e33
SnzDev <tms.working@gmail.com>
2023-03-04 14:15:28 -0300

    fixed hooks folder structure

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+import useDebounce from "./useDebounce";
+import useDisclose from "./useDisclose";
+import useDiscloseSelect from "./useDiscloseSelect";
+
+export { useDebounce, useDisclose, useDiscloseSelect };
